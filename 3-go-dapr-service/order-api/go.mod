f57cc02f2640a2fd6a6f20fdacf39ea5d7d41789
Fatih Işık <sir.fatih.isik@gmail.com>
2023-04-01 00:19:29 +0300

    started go dapr service examples

---
+++ 3-go-dapr-service/order-api/go.mod
@@ -0,0 +1,3 @@
+module github.com/sirfi/go-dapr-presentation/3-go-dapr-service/order-api
+
+go 1.20
---
+++ 3-go-dapr-service/order-api/main.go
@@ -0,0 +1,11 @@
+package main
+
+type OrderDto struct {
+	ID    string `json:"id"`
+	Item  string `json:"item"`
+	Price int    `json:"price"`
+}
+
+func main() {
+
+}
